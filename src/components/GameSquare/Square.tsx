52164983899483a6b454e86425603968a463aace
Azat Kulbaev <kulbaevaazat@gmail.com>
2023-10-29 21:42:24 +0600

    by click,square shows what is inside the box

--- src/components/GameSquare/Square.tsx
+++ src/components/GameSquare/Square.tsx
@@ -6,11 +6,25 @@ interface Props {
   isClicked: () => void;
 }
 
-const Square:React.FC<Props> = ({hasItem, clicked, isClicked}) => {
+const Square: React.FC<Props> = ({hasItem, clicked, isClicked}) => {
+  const boxStyle: React.CSSProperties = {
+    backgroundColor: 'grey', backgroundImage: 'none',
+    backgroundSize: 'none', backgroundRepeat: 'none',
+    backgroundPosition: 'none',
+  };
+
+  if (clicked) {
+    boxStyle.backgroundColor = 'transparent';
+  }
+
+  if (clicked && hasItem) {
+    boxStyle.backgroundImage = 'url("/public/gold.svg")';
+    boxStyle.backgroundRepeat = 'no-repeat';
+    boxStyle.backgroundPosition = 'center';
+  }
+
   return (
-    <div className={`square hasItem: ${hasItem} clicked: ${clicked}`} onClick={isClicked}>
-      <span>
-      </span>
+    <div style={boxStyle} className={`square hasItem:${hasItem} clicked:${clicked}`} onClick={isClicked}>
     </div>
   );
 };
